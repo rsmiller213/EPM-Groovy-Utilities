6caf4d97c1b3ae2e7dfcfbf786c4b38a4684347a
rsmiller213 <rsmiller213@gmail.com>
2020-06-26 18:32:50 -0400

    convert spaces from CM to tabs

--- BusinessRuleExamples/MustBeAttachedToDataForm/getGridDefFromMap.groovy
+++ BusinessRuleExamples/MustBeAttachedToDataForm/getGridDefFromMap.groovy
@@ -15,37 +15,37 @@ try {
 	// Get Mbr Map from Data Grid
 	Map<String,Map<String,List<String>>> mapGridMbrs = getEditedMbrMap(gridForm)
 	
-    // Map Before
-    println "Map Before : $mapGridMbrs"
-    // Update & Add Period to Map
-    ["Jul","Aug","Sep"].each { per ->
-    	mapGridMbrs["cols"]["Period"] << per
-    }
-    println "Map After Adding Dec : $mapGridMbrs"
+	// Map Before
+	println "Map Before : $mapGridMbrs"
+	// Update & Add Period to Map
+	["Jul","Aug","Sep"].each { per ->
+		mapGridMbrs["cols"]["Period"] << per
+	}
+	println "Map After Adding Dec : $mapGridMbrs"
 
-    DataGridDefinition gdForm 
-    DataGrid gridNew
-    
-    println "===== Simple Request with Logger"
-    gdForm = getGridDefFromMap(mapGridMbrs,true)
-    
-    println ""
-    
-    // Load & Log New Grid
-    gridNew = cube.loadGrid(gdForm, false)
-    logGrid(gridNew)
-    
-    println ""
-    
+	DataGridDefinition gdForm 
+	DataGrid gridNew
+	
+	println "===== Simple Request with Logger"
+	gdForm = getGridDefFromMap(mapGridMbrs,true)
+	
+	println ""
+	
+	// Load & Log New Grid
+	gridNew = cube.loadGrid(gdForm, false)
+	logGrid(gridNew)
+	
+	println ""
+	
 	println "===== Using Suppression Options"
-    gdForm = getGridDefFromMap(mapGridMbrs,true,[suppCols:true])
-    
-    println ""
-    
-    // Load & Log New Grid
-    gridNew = cube.loadGrid(gdForm, false)
-    logGrid(gridNew)
-    
+	gdForm = getGridDefFromMap(mapGridMbrs,true,[suppCols:true])
+	
+	println ""
+	
+	// Load & Log New Grid
+	gridNew = cube.loadGrid(gdForm, false)
+	logGrid(gridNew)
+	
 } catch (Exception e) {
 	println e
 } finally {
--- BusinessRuleExamples/MustBeAttachedToDataForm/getGridMbrMap.groovy
+++ BusinessRuleExamples/MustBeAttachedToDataForm/getGridMbrMap.groovy
@@ -4,36 +4,36 @@
 long startTime = currentTimeMillis() // get current time in milliseconds
 
 try {
-    // Grab the Cube & Application
-    Cube cube = rule.getCube()
-    Application app = cube.getApplication()
-    println "App : $app | Cube : $cube"
+	// Grab the Cube & Application
+	Cube cube = rule.getCube()
+	Application app = cube.getApplication()
+	println "App : $app | Cube : $cube"
 
-    // Get Grid from DataForm
-    DataGrid gridForm = operation.getGrid()
+	// Get Grid from DataForm
+	DataGrid gridForm = operation.getGrid()
 
-    // Initiate Map
-    Map<String,Map<String,List<String>>> mapGridMbrs = [:]
-    
+	// Initiate Map
+	Map<String,Map<String,List<String>>> mapGridMbrs = [:]
+	
 	mapGridMbrs = getGridMbrMap(gridForm)
-    println "Simple Request : getGridMbrMap(DataGrid) : $mapGridMbrs"
-    
-    println ""
-    
-    println "With Logger : getGridMbrMap(DataGrid, Boolean)"
-    mapGridMbrs = getGridMbrMap(gridForm,true)
-    
-    println ""
-    
-    mapGridMbrs = getGridMbrMap(gridForm,{DataCell cell -> cell.isMissing()})
-    println "With Custom Filter : getGridMbrMap(DataGrid, Predicate) - Filter is Missing Cells : $mapGridMbrs"
-    
-    println ""
-    
-    mapGridMbrs = getEditedMbrMap(gridForm)
-    println "Only Edited Members : getEditedMbrMap(DataGrid) : $mapGridMbrs"
-    
-    
+	println "Simple Request : getGridMbrMap(DataGrid) : $mapGridMbrs"
+	
+	println ""
+	
+	println "With Logger : getGridMbrMap(DataGrid, Boolean)"
+	mapGridMbrs = getGridMbrMap(gridForm,true)
+	
+	println ""
+	
+	mapGridMbrs = getGridMbrMap(gridForm,{DataCell cell -> cell.isMissing()})
+	println "With Custom Filter : getGridMbrMap(DataGrid, Predicate) - Filter is Missing Cells : $mapGridMbrs"
+	
+	println ""
+	
+	mapGridMbrs = getEditedMbrMap(gridForm)
+	println "Only Edited Members : getEditedMbrMap(DataGrid) : $mapGridMbrs"
+	
+	
 } catch (Exception e) {
 	println e
 }
